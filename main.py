0ae1d05cf3f5747510a60c1d749943be23d12f7f
riyaelyas162-commits <riyaelyas162@gmail.com>
2026-05-04 21:47:20 +0530

    Refactor bot commands to use async functions

--- main.py
+++ main.py
@@ -1,18 +1,27 @@
-from pyrogram import Client, filters
+import asyncio
+from pyrogram import Client, filters, idle
 from pyrogram.types import Message
 
-API_ID = int("34871535")
+API_ID = 34871535   # number (quotes nahi)
 API_HASH = "4e38cdb0eb2d2fb20aec1ed339a28c35"
-BOT_TOKEN = "8318890496:AAF1CN_dyMIXTe7Nexf_HMEElJ>"
+BOT_TOKEN = "8318890496:AAH91pPTBrt10Hxd1g-MXktRNxLVRxCT_nE"
 
 app = Client("music-bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 
 @app.on_message(filters.command("start"))
-def start(client, message: Message):
-    message.reply_text("🎵 Music Bot Started Successfully!")
+async def start(client, message: Message):
+    await message.reply_text("🎵 Music Bot Started Successfully!")j
 
 @app.on_message(filters.command("ping"))
-def ping(client, message: Message):
-    message.reply_text("🏓 Pong!")
+async def ping(client, message: Message):
+    await message.reply_text("🏓 Pong!")
 
-app.run()
+async def main():
+    await app.start()
+    print("Bot Started ✅")
+    await idle()
+
+if name == "main":
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    loop.run_until_complete(main())

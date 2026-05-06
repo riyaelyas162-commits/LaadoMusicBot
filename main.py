31cb77fe474ca7bc095e7b3d8e18bb63f35dfa7d
riyaelyas162-commits <riyaelyas162@gmail.com>
2026-05-06 14:45:50 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,49 @@
+import os
+from pyrogram import Client, filters
+from pytgcalls import PyTgCalls
+from pytgcalls.types.input_stream import AudioPiped
+
+API_ID = int(os.getenv("34871535"))
+API_HASH = os.getenv("4e38cdb0eb2d2fb20aec1ed339a28c35")
+BOT_TOKEN = os.getenv("8318890496:AAGkZKvOOknGLoZrDlxpxzFHyPxMjMqXrZw")
+SESSION = os.getenv("MIIBCgKCAQEA6LszBcC1LGzyr992NzE0>
+5nKaMBwK/BIb9xUfg0Q29/2mgIR6Zr9krM7HjuIcCzFvDtr>
+62cECs5HKhT5DZ98K33vmWiLowc621dQuwKWSQKjWf50XYF>
++aEyZ+uVgLLQbRA1dEjSDZ2iGRy12Mk5gpYc397aYp438fs>
+t6N/byY9Nw9p21Og3AoXSL2q/2IJ1WRUhebgAdGVMlV1fku>
+5+bfo3Nhmcyvk5ftB0WkJ9z6bNZ7yxrP8wIDAQAB")
+
+bot = Client("bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
+assistant = Client("assistant", api_id=API_ID, api_hash=API_HASH, session_string=SESSION)
+
+call = PyTgCalls(assistant)
+
+@bot.on_message(filters.command("start"))
+async def start(_, m):
+    await m.reply("🎵 VC Music Bot Ready!")
+
+@bot.on_message(filters.command("play") & filters.reply)
+async def play(_, m):
+    audio = m.reply_to_message.audio or m.reply_to_message.voice
+    if not audio:
+        return await m.reply("❌ Audio reply karo")
+
+    file = await m.reply_to_message.download()
+
+    await call.join_group_call(
+        m.chat.id,
+        AudioPiped(file)
+    )
+    await m.reply("▶️ Playing in VC...")
+
+@bot.on_message(filters.command("stop"))
+async def stop(_, m):
+    await call.leave_group_call(m.chat.id)
+    await m.reply("⏹️ Stopped")
+
+bot.start()
+assistant.start()
+call.start()
+
+import asyncio
+asyncio.get_event_loop().run_forever()
